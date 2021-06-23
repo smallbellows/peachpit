bd06b56e9b2fd43d7ee3b6a54373e034b0eaba58
Sara <given.sara@gmail.com>
2021-06-23 06:07:41 -0700

    fix request url

--- src/components/Auth/RequestForm.tsx
+++ src/components/Auth/RequestForm.tsx
@@ -10,7 +10,7 @@ const encode = (data: any) => {
     return Object.keys(data)
         .map(
             (key) =>
-                encodeURIComponent(key) + '+' + encodeURIComponent(data[key])
+                encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
         )
         .join('&');
 };
